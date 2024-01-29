6bb714ffae1f8d65b4568a50bea12012a8896a92
JoelMndz <luisjo3lml@gmail.com>
2024-01-29 13:42:10 -0500

    Consumir API

--- lib/data/repository/bancos.dart
+++ lib/data/repository/bancos.dart
@@ -1,13 +1,27 @@
 import 'dart:convert';
 
+import 'package:dio/dio.dart';
 import 'package:ecomap/domain/domain.dart';
 import 'package:flutter/services.dart';
 
 class BancosRepository extends BancosBaseRepository{
+  final API = "https://api-ecomap.onrender.com/api/bancos";
+
   @override
   Future<Map<String, dynamic>> getAll() async {
-    String jsonString = await rootBundle.loadString('assets/json/bancos.json');
-    return jsonDecode(jsonString);
+    Map<String, dynamic>? data;
+    try {
+      final response = await Dio().get(API);
+      data = response.data;
+      print(data);
+    } catch (e) {
+      print(e.toString());
+    }
+    if(data == null){
+      String jsonString = await rootBundle.loadString('assets/json/bancos.json');
+      data = jsonDecode(jsonString);
+    }
+    return data!;   
   }
 
 }
--- lib/data/repository/provincias.dart
+++ lib/data/repository/provincias.dart
@@ -1,14 +1,28 @@
 
 import 'dart:convert';
 
+import 'package:dio/dio.dart';
 import 'package:ecomap/domain/domain.dart';
 import 'package:flutter/services.dart';
 
 class ProvinciasRepository extends ProvinciaBaseRepository{
+  final API = "https://api-ecomap.onrender.com/api/provincias";
+  
   @override
   Future<Map<String, dynamic>> getAll() async {
-    String jsonString = await rootBundle.loadString('assets/json/provincias.json');
-    return jsonDecode(jsonString);
+    Map<String, dynamic>? data;
+    try {
+      final response = await Dio().get(API);
+      data = response.data;
+      print(data);
+    } catch (e) {
+      print(e.toString());
+    }
+    if(data == null){
+      String jsonString = await rootBundle.loadString('assets/json/provincias.json');
+      data = jsonDecode(jsonString);
+    }
+    return data!;
   }
 
 }
